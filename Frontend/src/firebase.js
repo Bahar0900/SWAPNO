cc2047796680fba9489e8d28199a66f4dbff342a
bahar0900 <sagormdsagorchowdhury@gmail.com>
2026-09-02 18:49:10 +0600

    Initial commit

--- Frontend/src/firebase.js
+++ Frontend/src/firebase.js
@@ -2,14 +2,7 @@ import { initializeApp } from 'firebase/app';
 import { getAuth, GoogleAuthProvider, signInWithPopup, signOut, onAuthStateChanged } from 'firebase/auth';
 
 // From Firebase Console -> Project settings -> Your apps -> Web app
-const firebaseConfig = {
-  apiKey: "AIzaSyBWuVTBZZvNX1Ggw2WH5dVI6iqr6wXyGkE",
-  authDomain: "shwapno-store.firebaseapp.com",
-  projectId: "shwapno-store",
-  storageBucket: "shwapno-store.firebasestorage.app",
-  messagingSenderId: "887017636703",
-  appId: "1:887017636703:web:c379bd640b1ed843ffacea"
-};
+
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
